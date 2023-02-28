39acdfa80c9fcdeeecc29510afe575a88cd3e615
Berk TURKUCU <95139164+turkucuberk@users.noreply.github.com>
2023-02-28 21:04:32 +0300

    .

--- specs/android-ios-web-test.js
+++ specs/android-ios-web-test.js
@@ -1,10 +1,17 @@
 describe("Web automation real device", () => {
     it("Changes color", async () => {
+      browser.setTimeout({ 'script': 60000 });
       browser.url("https://mfml.in/api/getInfo")
       var color = await $("id=resolution");
       await color.waitForDisplayed({ timeout: 30000 });
       await color.click();
       await color.click();
+    });
+
+    it('Should get all window handles', async () => {
+      //   await driver.setAsyncScriptTimeout(5000);
+         windowHandles = await browser.getWindowHandles();
+         console.log(windowHandles);
     });
   
     it("Changes text", async () => {
